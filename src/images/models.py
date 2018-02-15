7a5bc35daf295bd3cecb91d9bf63cfb6d21322b1
seb <harinck.sebastien@gmail.com>
2018-02-15 15:49:05 +0100

    add Tag Model

--- src/images/models.py
+++ src/images/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
 class Image(models.Model):
     file = models.ImageField()
+    tags = models.ManyToManyField(Tag)
