bc396396a776f4c6316a7d0c85c32ba24106210a
seb <harinck.sebastien@gmail.com>
2018-02-16 10:50:22 +0100

    create a setup for images test

--- src/images/tests.py
+++ src/images/tests.py
@@ -5,43 +5,47 @@ from .models import Image, Tag, get_random_image_without_tags
 
 
 class ImageModelTests(TestCase):
+    def setUp(self):
+        Image.objects.create(file='img_a.jpeg')
+        Image.objects.create(file='img_b.jpeg')
+        Tag.objects.create(name='kitchen')
+        Tag.objects.create(name='bathroom')
+        Tag.objects.create(name='bedroom')
+
     def test_display_img(self):
         """
         A user can display an Image Model.
         """
-        image = Image.objects.create(file='img.jpeg')
-        response = self.client.get(reverse('images:detail', args=(image.id,)))
+        image_a = Image.objects.get(file='img_a.jpeg')
+        response = self.client.get(reverse('images:detail', args=(image_a.id,)))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'img.jpeg')
+        self.assertContains(response, 'img_a.jpeg')
 
     def test_add_tags(self):
         """
         A user can add tags to an image
         """
-        image = Image.objects.create(file='img.jpeg')
-        kitchen = Tag.objects.create(name='kitchen')
-        bathroom = Tag.objects.create(name='bathroom')
-        bedroom = Tag.objects.create(name='bedroom')
+        image_a = Image.objects.get(file='img_a.jpeg')
+        kitchen = Tag.objects.get(name='kitchen')
+        bedroom = Tag.objects.get(name='bedroom')
 
         self.client.post(
-            reverse('images:add_tags', args=(image.id,)),
+            reverse('images:add_tags', args=(image_a.id,)),
             {'tags': (kitchen.id, bedroom.id)}
         )
 
         expected_result = list(map(repr, [bedroom, kitchen]))
-        self.assertQuerysetEqual(image.tags.order_by('name'), expected_result)
+        self.assertQuerysetEqual(image_a.tags.order_by('name'), expected_result)
 
     def test_random_redirection(self):
         """
         When a user add tags to an image, he must to be redirected
         on the new image to classify
         """
-        image_a = Image.objects.create(file='img_a.jpeg')
-        image_b = Image.objects.create(file='img_b.jpeg')
+        image_a = Image.objects.get(file='img_a.jpeg')
+        image_b = Image.objects.get(file='img_b.jpeg')
 
-        Tag.objects.create(name='kitchen')
-        bathroom = Tag.objects.create(name='bathroom')
-        Tag.objects.create(name='bedroom')
+        bathroom = Tag.objects.get(name='bathroom')
 
         response = self.client.post(
             reverse('images:add_tags', args=(image_a.id,)),
