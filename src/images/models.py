d4701c258beea843ae3341d9ef584d7c0145756c
Sébastien Harinck <harinck.sebastien@gmail.com>
2018-03-15 14:40:17 +0100

    refactoring

--- src/images/models.py
+++ src/images/models.py
@@ -4,12 +4,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
 
-from buckets.models import *
-
 
 class Image(models.Model):
     file = models.ImageField()
-    bucket = models.ForeignKey(Bucket, on_delete=models.DO_NOTHING)
+    bucket = models.ForeignKey('buckets.Bucket', on_delete=models.DO_NOTHING)
 
     def get_absolute_url(self):
         return reverse('images:detail', kwargs={'pk': self.pk})
@@ -18,8 +16,7 @@ class Image(models.Model):
 class Vote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    # bucket = models.ForeignKey(Bucket, on_delete=models.DO_NOTHING)
-    labels = models.ManyToManyField(Label)
+    labels = models.ManyToManyField('buckets.Label')
     created_at = models.DateTimeField(auto_now_add=True)
 
 
---
+++ src/images/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class VoteCreateViewTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_success_url(self):
+        pass
+        # self.client.get(reverse('images:vote'))
