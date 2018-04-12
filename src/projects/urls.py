66871064cb9275e630a87f093573588d556413f4
Sébastien Harinck <harinck.sebastien@gmail.com>
2018-04-12 10:29:53 +0200

    create project detail view

--- src/projects/urls.py
+++ src/projects/urls.py
@@ -6,4 +6,5 @@ app_name = 'projects'
 
 urlpatterns = [
     path('', views.ProjectListView.as_view(), name='list'),
+    path('<int:pk>/', views.ProjectDetailView.as_view(), name='detail'),
 ]
--- src/projects/views.py
+++ src/projects/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Project
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
+    context_object_name = 'projects'
 
     def get_queryset(self):
         return Project.objects.filter(user=self.request.user)
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
+    context_object_name = 'project'
