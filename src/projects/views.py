9918cf557d3014e4470f60b01d7b12ce1d283231
Sébastien Harinck <harinck.sebastien@gmail.com>
2018-04-12 10:18:38 +0200

    projects lists display only the projects of the user

--- src/projects/views.py
+++ src/projects/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from .models import Project
 
 
-class ProjectsListView(ListView):
+class ProjectsListView(LoginRequiredMixin, ListView):
     model = Project
+
+    def get_queryset(self):
+        return Project.objects.filter(user=self.request.user)
